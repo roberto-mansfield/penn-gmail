3ec091d554f8e2a8d75795cf76af752c131fcccb
Roberto Mansfield <robertom@sas.upenn.edu>
2014-05-02 16:54:44 -0400

    Add Google Admin PHP api to vendors

--- app/autoload.php
+++ app/autoload.php
@@ -8,6 +8,9 @@ use Composer\Autoload\ClassLoader;
  */
 $loader = require __DIR__.'/../vendor/autoload.php';
 
+// add google root manually since the Google libs rely on "require_once" instead of an autoloader
+set_include_path(get_include_path() . PATH_SEPARATOR . __DIR__ . '/../vendor/google/google-api-php-client/src');
+
 AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
 
 return $loader;
