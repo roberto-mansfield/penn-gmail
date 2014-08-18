734c9d9ab8fdbf65a1be4e36115de87111f262b4
Roberto Mansfield <robertom@sas.upenn.edu>
2014-08-18 10:26:29 -0400

    Update javascript on edit name

--- src/SAS/IRAD/GMailConfigureBundle/Resources/public/js/googleEditName.js
+++ src/SAS/IRAD/GMailConfigureBundle/Resources/public/js/googleEditName.js
@@ -1,38 +1,62 @@
 $(function() {
-	$("input#updateNameButton").on("click", validateName);
+	updateNameSetup();
 });
 
 
-function validateName() {
+updateNameSetup = (function() {
 
-	var form      = $("form#updateName");
-    var firstName = form.find("input.firstNameField").val();
-    var lastName  = form.find("input.lastNameField").val();
-    var button    = form.find("input#updateNameButton");
+	var self = {};
 	
-	if ( !firstName ) {
-		alert("Please enter a first name.");
-		return;
+	function setup() {
+		self.form      = $("form#updateName");
+	    self.firstName = self.form.find("input.firstNameField");
+	    self.lastName  = self.form.find("input.lastNameField");
+	    self.button    = self.form.find("button#updateNameButton");
+	    self.progress  = $("img#progress-img");
+		
+	    self.button.on("click", validateName);
 	}
-
-    var invalidChars = /[\<\>\"\&\%]/;
-
-    if ( invalidChars.test(firstName) ) {
-        alert("Your first name contains invalid characters.");
-        return;
-    }
 	
-	if ( !lastName ) {
-		alert("Please enter a last name.");
-		return;
+
+	function validateName() {
+	
+	    var invalidChars = /[\<\>\"\&\%]/;
+
+	    if ( !self.firstName.val() ) {
+			alert("Please enter a first name.");
+			return;
+		}
+	
+	    if ( invalidChars.test(self.firstName.val()) ) {
+	        alert("Your first name contains invalid characters.");
+	        return;
+	    }
+		
+		if ( !self.lastName.val() ) {
+			alert("Please enter a last name.");
+			return;
+		}
+	
+	    if ( invalidChars.test(self.lastName.val()) ) {
+	        alert("Your last name contains invalid characters.");
+	        return;
+	    }
+		
+	    freezeForm();
+		self.form.submit();
 	}
-
-    if ( invalidChars.test(lastName) ) {
-        alert("Your last name contains invalid characters.");
-        return;
-    }
 	
-    button.val("Updating...").prop('disabled', true);
-    
-	form.submit();
-}
+	function freezeForm() {
+		self.button.prop('disabled', true)
+			.text('Processing ')
+			.append( self.progress.clone().show() );
+		self.form.find("input").prop("readonly", true);
+	}
+	
+	function thawForm() {
+		self.button.prop('disabled', false).text('Update');
+		self.form.find("input").prop("readonly", false);
+	}
+	
+	return setup;
+})();
