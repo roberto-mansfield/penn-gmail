eede2f55e6fef7ea70a87378ccd035727e499c1e
Roberto Mansfield <robertom@sas.upenn.edu>
2014-09-17 11:04:17 -0400

    Fix entity already exists exception test in BulkCreateAccounts

--- src/SAS/IRAD/BulkCreateAccountsBundle/Command/BulkCreateAccountsCommand.php
+++ src/SAS/IRAD/BulkCreateAccountsBundle/Command/BulkCreateAccountsCommand.php
@@ -88,7 +88,7 @@ class BulkCreateAccountsCommand extends ContainerAwareCommand {
                         $account->setCreated(true);
                     } catch (\Exception $e) {
                         $helper->errorLog("Exception occurred creating account for penn_id: $penn_id, pennkey: " . $account->getPennkey() . ", error: " . $e->getMessage());
-                        if ( preg_match("", $e->getMessage()) ) {
+                        if ( preg_match("/\(409\) Entity already exists/", $e->getMessage()) ) {
                             // if entity already exists, update cache
                             $account->setCreated(true);
                         }
